8db9098edf90eb8414ff247a17380f191a6b4496
mouad <155776432+xpowder@users.noreply.github.com>
2024-05-03 15:45:06 +0100

    Update urls.py

--- System/loginapp/urls.py
+++ System/loginapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    # Add other URL patterns as needed
+    
 ]
